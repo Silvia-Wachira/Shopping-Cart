d8e768d0202d819b4e5b6be3c4f4b4a131cdb6f0
Silvia-Wachira <slywachira96@gmail.com>
2023-08-28 22:04:23 +0300

    create an evet listener

--- js/app.js
+++ js/app.js
@@ -5,7 +5,12 @@ const courses = document.querySelector('#courses-list')
 
 //listeners
 
+loadEventListeners()
 
+function loadEventListeners() {
+    //when a new course is added
+    courses.addEventListener('click',buyCourse)
+}
 
 
 
